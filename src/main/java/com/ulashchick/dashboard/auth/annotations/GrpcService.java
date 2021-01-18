b9cc24c4e2890eb1914056b003fedc47af5557bc
Alexey Ulashchick <a.ulashckick@gmail.com>
2021-01-17 20:10:24 -0800

    Basic application template.

---
+++ src/main/java/com/ulashchick/dashboard/auth/annotations/GrpcService.java
@@ -0,0 +1,8 @@
+package com.ulashchick.dashboard.auth.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface GrpcService {
+}
