f6f4dd0eff01fb74da57f30b4020d89f7aeafab5
Alexey Ulashchick <a.ulashckick@gmail.com>
2021-01-23 23:59:49 -0800

    Testing gRPC with proxying through Envoy.

--- src/main/java/com/ulashchick/dashboard/auth/persistance/CassandraSession.java
+++ src/main/java/com/ulashchick/dashboard/auth/persistance/CassandraSession.java
@@ -5,6 +5,7 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.ulashchick.dashboard.auth.config.ConfigService;
 import java.io.IOException;
+import org.slf4j.Logger;
 
 @Singleton
 public class CassandraSession {
@@ -12,14 +13,25 @@ public class CassandraSession {
   @Inject
   private ConfigService configService;
 
-  private final CqlSession session;
+  @Inject
+  private Logger logger;
 
-  public CassandraSession() throws IOException {
-    session = CqlSession.builder()
-        .withLocalDatacenter(CassandraKeyspace.DATACENTER)
-        .addContactPoints(configService.getCassandraEndpoints())
-        .withKeyspace(CassandraKeyspace.KEYSPACE)
-        .build();
+  private final CqlSession session = null;
+
+  public CassandraSession() {
+//    CqlSession session;
+//    try {
+//      session = CqlSession.builder()
+//          .withLocalDatacenter(CassandraKeyspace.DATACENTER)
+//          .addContactPoints(configService.getCassandraEndpoints())
+//          .withKeyspace(CassandraKeyspace.KEYSPACE)
+//          .build();
+//    } catch (IOException e) {
+//      logger.error("Cannot connect to Cassandra cluster", e);
+//      session = null;
+//    }
+//
+//    this.session = session;
   }
 
   public CqlSession getSession() {
--- src/main/java/com/ulashchick/dashboard/auth/services/AuthService.java
+++ src/main/java/com/ulashchick/dashboard/auth/services/AuthService.java
@@ -4,13 +4,9 @@ import com.google.inject.Inject;
 import com.ulashchick.dashboard.auth.annotations.GrpcService;
 import com.ulashchick.dashboard.auth.persistance.CassandraClient;
 import io.reactivex.Single;
-import javax.annotation.Nonnull;
-import org.apache.commons.lang3.RandomStringUtils;
-import protos.com.dashboard.ulashchick.auth.RegisterUserRequest;
-import protos.com.dashboard.ulashchick.auth.RegisterUserResponse;
 import protos.com.dashboard.ulashchick.auth.RxAuthServiceGrpc;
-import protos.com.dashboard.ulashchick.auth.ValidateUserRequest;
-import protos.com.dashboard.ulashchick.auth.ValidateUserResponse;
+import protos.com.dashboard.ulashchick.auth.SignInUserRequest;
+import protos.com.dashboard.ulashchick.auth.SignInUserResponse;
 
 @GrpcService
 public class AuthService extends RxAuthServiceGrpc.AuthServiceImplBase {
@@ -19,27 +15,9 @@ public class AuthService extends RxAuthServiceGrpc.AuthServiceImplBase {
   CassandraClient cassandraClient;
 
   @Override
-  public Single<RegisterUserResponse> registerUser(Single<RegisterUserRequest> request) {
+  public Single<SignInUserResponse> signIn(Single<SignInUserRequest> request) {
     return request
-        .map(RegisterUserRequest::getEmail)
-        .map(this::createUserWithEmailReturnTempPassword)
-        .map(tempPassword -> RegisterUserResponse.getDefaultInstance());
-  }
-
-  @Override
-  public Single<ValidateUserResponse> validateUser(Single<ValidateUserRequest> request) {
-    return super.validateUser(request);
-  }
-
-  /**
-   * Generate random password and creates user with provided email and generated password.
-   * Returns back single of generated raw password. This might be send as a part of the return
-   * link with generated email.
-   */
-  private Single<String> createUserWithEmailReturnTempPassword(@Nonnull String email) {
-    final String tempPassword = RandomStringUtils.random(16, true, true);
-
-    return cassandraClient.insertIntoUserByEmail(email, tempPassword, false)
-        .map(r -> tempPassword);
+        .map(r -> String.format("%s-%s-%s", r.getEmail(), r.getFirstName(), r.getLastName()))
+        .map(message -> SignInUserResponse.newBuilder().setJwtToken(message).build());
   }
 }
