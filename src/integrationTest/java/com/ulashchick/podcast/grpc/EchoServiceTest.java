8bb5f8cf9de21be714bc0e5983354187cf673dd4
Alexey Ulashchick <a.ulashchick@gmail.com>
2021-11-17 08:42:37 -0800

    Local development enhancements. Build time around 5 seconds and support of debugging.

--- src/integrationTest/java/com/ulashchick/podcast/grpc/EchoServiceTest.java
+++ src/integrationTest/java/com/ulashchick/podcast/grpc/EchoServiceTest.java
@@ -7,27 +7,36 @@ import io.grpc.Server;
 import io.grpc.inprocess.InProcessChannelBuilder;
 import io.grpc.testing.GrpcCleanupRule;
 import org.junit.Rule;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import protos.com.ulashchick.podcast.echo.EchoRequest;
 import protos.com.ulashchick.podcast.echo.EchoResponse;
 import protos.com.ulashchick.podcast.echo.EchoServiceGrpc;
 
+import javax.annotation.Nonnull;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import static com.google.common.truth.Truth.assertThat;
 
 @ExtendWith(TestParameterResolver.class)
 class EchoServiceTest {
 
+  private final Logger logger;
+
   @Rule
   public final GrpcCleanupRule grpcCleanup = new GrpcCleanupRule();
 
-  @Test
-  void authorizationIsNotRequired() throws IOException {
-    final String basePackage = getClass().getPackage().getName();
+  public EchoServiceTest(@Nonnull Logger logger) {
+    this.logger = logger;
+  }
 
-    ApplicationServerBuilder applicationServerBuilder = DependencyManager.getInstance(ApplicationServerBuilder.class);
+  @BeforeEach
+  void beforeEach() throws IOException {
+    final String basePackage = getClass().getPackage().getName();
+    final ApplicationServerBuilder applicationServerBuilder = DependencyManager
+        .getInstance(ApplicationServerBuilder.class);
 
     final Server server = applicationServerBuilder.forTest("test-server")
         .addServices(basePackage)
@@ -37,10 +46,17 @@ class EchoServiceTest {
 
     grpcCleanup.register(server);
 
+    logger.info("Server starter. Sending request.");
+  }
+
+  @Test
+  void authorizationIsNotRequired() {
     final EchoServiceGrpc.EchoServiceBlockingStub echoServiceBlockingStub = EchoServiceGrpc
         .newBlockingStub(grpcCleanup.register(InProcessChannelBuilder.forName("test-server").directExecutor().build()));
 
     EchoResponse hello = echoServiceBlockingStub.echo(EchoRequest.newBuilder().setMessage("Hello").build());
+
+    logger.info("Response received.");
 
     assertThat(hello.getMessage()).endsWith("Hello");
   }
