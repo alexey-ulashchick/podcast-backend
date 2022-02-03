5ac228fc475c6de999b32c295a4fc4f81b0dd4d3
Alexey Ulashchick <a.ulashchick@gmail.com>
2022-02-03 12:19:41 -0800

    Added .env files support.

--- src/main/java/com/ulashchick/podcast/common/config/EnvironmentService.java
+++ src/main/java/com/ulashchick/podcast/common/config/EnvironmentService.java
@@ -6,55 +6,60 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import java.util.Arrays;
 
 @Singleton
 public class EnvironmentService {
-    private static final Logger logger = LoggerFactory.getLogger(ConfigService.class);
-    private final Dotenv dotenv;
+  private static final Logger logger = LoggerFactory.getLogger(EnvironmentService.class);
+  private final Dotenv dotenv;
 
-    public enum Environment {
-        DEV("DEV"),
-        TEST("TEST"),
-        PROD("PROD");
+  public enum Environment {
+    DEV("DEV"),
+    TEST("TEST"),
+    PROD("PROD");
 
-        private final String label;
+    private final String label;
 
-        Environment(String label) {
-            this.label = label;
-        }
-
-        public String getLabel() {
-            return label;
-        }
+    Environment(String label) {
+      this.label = label;
     }
 
-    public EnvironmentService() {
-        dotenv = Dotenv.configure().load();
+    public String getLabel() {
+      return label;
     }
+  }
 
-    /**
-     * Checks current system environment through system ENV environment variable. When not set,
-     * default environment will be set to {@code Environment.DEV}.
-     */
-    @Nonnull
-    public Environment getCurrentEnvironment() {
-        final String env = readEnvVariable("ENV");
-        return Arrays.stream(Environment.values())
-                .filter(item -> item.label.equalsIgnoreCase(env))
-                .findFirst()
-                .orElse(Environment.DEV);
-    }
+  public EnvironmentService() {
+    dotenv = Dotenv.configure().load();
+  }
 
-    @Nonnull
-    public String getCurrentEnvironmentAsString() {
-        return getCurrentEnvironment().getLabel().toLowerCase();
-    }
+  /**
+   * Checks current system environment through system ENV environment variable. When not set,
+   * default environment will be set to {@code Environment.DEV}.
+   */
+  @Nonnull
+  public Environment getCurrentEnvironment() {
+    final String env = readEnvVariable("ENV");
+    return Arrays.stream(Environment.values())
+        .filter(item -> item.label.equalsIgnoreCase(env))
+        .findFirst()
+        .orElse(Environment.DEV);
+  }
 
-    @Nonnull
-    public String readEnvVariable(@Nonnull String variable) {
-        return dotenv.get(variable);
-    }
+  @Nonnull
+  public String getCurrentEnvironmentAsString() {
+    return getCurrentEnvironment().getLabel().toLowerCase();
+  }
+
+  @Nonnull
+  public String readEnvVariable(@Nonnull String variable) {
+      final String value = dotenv.get(variable);
+
+      if (value.isEmpty()) {
+          logger.warn("Environment variable {} has not been set.", variable);
+      }
+
+      return value;
+  }
 
 }
